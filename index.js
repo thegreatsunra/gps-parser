d8b78b2db2969b6c822bc1dbe2c1ccbeedd3f04e
Dane Petersen <thegreatsunra@gmail.com>
2018-02-14 14:46:15 -0600

    Start serialport and listen at "default" port

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const SerialPort = require('serialport')
 const nmea = require('nmea')
 
+const device = process.argv[2] || '/dev/tty.usbmodem14321'
+
+const port = new SerialPort(device, {
+  baudRate: 9600
+})
+
+console.log(`listening for serial data at ${device}`)
+console.log('')
+
