0a4325503a52acbc19ed093f37aeb5572c6ac4b3
Dane Petersen <thegreatsunra@gmail.com>
2018-02-14 14:47:38 -0600

    Use nmea and only parse the GPS "sentences" we care about

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const SerialPort = require('serialport')
 const nmea = require('nmea')
 
+// this is the only GPS "sentence" we care about
+const sentence = '$GPGGA'
 const device = process.argv[2] || '/dev/tty.usbmodem14321'
 
 const port = new SerialPort(device, {
@@ -12,5 +14,11 @@ console.log('')
 
 port.on('data', (line) => {
   line = line.toString()
+  // only parse the GPS sentence we care about... ignore the others
+  if (line.substring(0, sentence.length) === sentence) {
+    const parsed = nmea.parse(line)
+    console.log(parsed)
+    console.log('')
+  }
 })
 
